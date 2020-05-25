85a8c09440a033a04b3d663c32b9ddd4b6d2ac15
essa <essa.abouasali@sutdytafensw.edu.au>
2020-05-25 16:02:30 +1000

    todays work

---
+++ src/GetTheWeather.js
@@ -0,0 +1,33 @@
+const axios = require("axios");
+
+const { weatherapikey } = require("../config/Keys");
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+function askQuestion(question) {
+    let answer;
+
+    return new Promise((resolve, reject) => {
+        rl.question(question, (ans) => {
+            resolve(ans);
+        })
+    });
+}
+
+
+async function Program() {
+    
+    let city = "Sydney";
+    let response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${weatherapikey}`)
+    console.log(`the maximum temperature in ${response.data.name} is ${response.data.main.temp_max - 273.1}`);
+    //console.log(response.data);
+}
+
+Program().then(() => {
+    process.exit(0);
+});
+
---
+++ src/VariablePointers.js
@@ -0,0 +1,16 @@
+// Dereference Array
+
+
+// let myArr = [1,2,4];
+// let myArrcopy = new Array();
+// myArrcopy = myArr.map(num => num);
+// myArrcopy.push(6);
+// console.log(myArr);
+// console.log(myArrcopy);
+
+// SPREAD Syntax
+
+let myArr = [1,2,3];
+let myrr2 = [12,13,14];
+let myarrycopy = [10,...myArr,...myrr2];
+
